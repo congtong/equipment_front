c9a414f97730ce2c88ae0a55f147ae99fc4c1b8a
cong.tong <276620500@qq.com>
2018-08-27 12:59:04 +0800

    新写less文件

--- raw/assets/js/equipment.js
+++ raw/assets/js/equipment.js
@@ -1,13 +1,38 @@
 (function (window) {
     $(document).ready(function(){
-        $(".panel-title a").click(function(){
-            var className = $(this).children()[0].className;
+        $('.padding-menu').click(function(){
+            var className = $(this).children().children().children()[0].className;
             if(className =='fa fa-angle-down float-right'){
                 className = 'fa fa-angle-right float-right';
             }else{
                 className = "fa fa-angle-down float-right";
             }
-            $(this).children()[0].className = className;
+            $(this).children().children().children()[0].className = className;
+        })
+    });
+
+    $('body').on('submit', 'form[action^="gini-ajax:"]', function (e) {
+        if ($(this).data('delegated')) return false;
+
+        e.preventDefault();
+
+        var $form = $(this);
+
+        $form.trigger('ajax-before');
+
+        $.ajax({
+            type: $form.attr('method') || "POST",
+            url: $form.attr('action').substring(10),
+            data: $form.serialize(),
+            success: function (html) {
+                $form.trigger('ajax-success', html);
+                $('body').append(html).find('script[data-ajax]').remove();
+            },
+            complete: function () {
+                $form.trigger('ajax-complete');
+            }
         });
+
+        return false;
     });
 })(window);
